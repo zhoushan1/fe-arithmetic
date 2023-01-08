b49bf8358a47c86bec8b092b1b85b43b92986c81
周珊 <672563682@qq.com>
2023-01-08 12:37:10 +0800

    feat: add

---
+++ 2.全排列/sku.js
@@ -0,0 +1,13 @@
+/*
+ * 电商sku全排列
+ 输入：["iPhone X", "iPhone XS"], ["黑色", "白色"], ["64g", "256g"] 三个数组
+ 输出：[  ["iPhone X", "黑色", "64g"],
+  ["iPhone X", "黑色", "256g"],
+  ["iPhone X", "白色", "64g"],
+  ["iPhone X", "白色", "256g"],
+  ["iPhone XS", "黑色", "64g"],
+  ["iPhone XS", "黑色", "256g"],
+  ["iPhone XS", "白色", "64g"],
+  ["iPhone XS", "白色", "256g"],
+]
+ */
